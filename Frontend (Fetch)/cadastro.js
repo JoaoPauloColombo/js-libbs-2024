d155e9ec510472eae4ef791e65f2c4ab77473a99
João Paulo <joaosouzapaulo446@gmail.com>
2024-05-13 16:25:40 -0300

    FETCH
    Criação do fetch

---
+++ Frontend (Fetch)/cadastro.js
@@ -0,0 +1,28 @@
+const form = document.getElementById("form");
+
+form.addEventListener("submit", function (e) {
+
+    const name = document.getElementById("name");
+    const email = document.getElementById("email");
+    const password = document.getElementById("password");
+
+    e.preventDefault();
+
+    const userData = JSON.stringify({
+        name: name.value,
+        email: email.value,
+        password: password.value,
+    });
+
+    fetch("http://10.92.198.38:8080/auth/singup", {
+        method: "POST",
+        body: userData,
+        headers:{
+            "Content-Type": "application/json; charset=utf-8"
+        }
+    })
+    .then((result) => result.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+    
+})
